657d0798899ef8aa2400055a7fe4be9edf2ce498
odnaglaziw <odnaglazik@gmail.com>
2024-05-28 13:56:11 +0500

    Сделал 4 вариант

--- app/src/main/java/com/bignerdranch/android/savessave/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/savessave/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.bignerdranch.android.savessave
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bignerdranch.android.savessave.ui.theme.SavesSaveTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,21 +14,26 @@ class MainActivity : ComponentActivity() {
 
         val preferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
         val editor = preferences.edit()
+        val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
+        val launchCountTextView = findViewById<TextView>(R.id.launchCountTextView)
 
-        // Получаем количество запусков
         val launchCount = preferences.getInt("launch_count", 0) + 1
 
-        if (launchCount != 0 && launchCount % 2 == 0) {
-            editor.putInt("launch_count", launchCount)
-            findViewById<TextView>(R.id.launchCountTextView).text = "Launch count: $launchCount"
-        } else if (launchCount != 1) {
-            editor.putInt("launch_count", 0)
-            findViewById<TextView>(R.id.launchCountTextView).text = "Launch count: 0"
-        } else{
-            editor.putInt("launch_count", launchCount)
-            findViewById<TextView>(R.id.launchCountTextView).text = "Launch count: 1"
+        when {
+            launchCount == 1 -> {
+                mainLayout.setBackgroundColor(Color.RED)
+                launchCountTextView.text = "Launch count: 1"
+            }
+            launchCount % 2 == 0 -> {
+                mainLayout.setBackgroundColor(Color.GREEN)
+                launchCountTextView.text = "Launch count: $launchCount"
+            }
+            else -> {
+                mainLayout.setBackgroundColor(Color.BLUE)
+                launchCountTextView.text = "Launch count: $launchCount"
+            }
         }
-
+        editor.putInt("launch_count", launchCount)
         editor.apply()
     }
 }
