677d5949ea0fc0108cfa8c81bcca0e34282072b5
odnaglaziw <odnaglazik@gmail.com>
2024-05-30 12:39:29 +0500

    Оказалось у меня не 4 вариант

--- app/src/main/java/com/bignerdranch/android/savessave/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/savessave/MainActivity.kt
@@ -12,28 +12,5 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val preferences = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-        val editor = preferences.edit()
-        val mainLayout = findViewById<LinearLayout>(R.id.mainLayout)
-        val launchCountTextView = findViewById<TextView>(R.id.launchCountTextView)
-
-        val launchCount = preferences.getInt("launch_count", 0) + 1
-
-        when {
-            launchCount == 1 -> {
-                mainLayout.setBackgroundColor(Color.RED)
-                launchCountTextView.text = "Launch count: 1"
-            }
-            launchCount % 2 == 0 -> {
-                mainLayout.setBackgroundColor(Color.GREEN)
-                launchCountTextView.text = "Launch count: $launchCount"
-            }
-            else -> {
-                mainLayout.setBackgroundColor(Color.BLUE)
-                launchCountTextView.text = "Launch count: $launchCount"
-            }
-        }
-        editor.putInt("launch_count", launchCount)
-        editor.apply()
     }
 }
